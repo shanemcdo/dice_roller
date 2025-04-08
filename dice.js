be41d97bc8d98369040940af95e46eef52bfd62a
Shane McDonough <shanemcdman@gmail.com>
2025-04-08 12:48:00 -0700

    Add advantage/disadvantage for dice rolls

--- dice.js
+++ dice.js
@@ -1,3 +1,9 @@
+const RollType = Object.freeze({
+	STANDARD: 'Standard',
+	ADVANTAGE: 'Advantage',
+	DISADVANTAGE: 'Disadvantage',
+});
+
 function get_std_die(faces){
     return ()=>Math.ceil(Math.random() * faces);
 }
@@ -10,16 +16,25 @@ function roll_d100(){
     return roll_std_die(10) * 10;
 }
 
-function roll_die(n){
-    if(n == 100)
-        return roll_d100();
-    return roll_std_die(n);
+function roll_die(n, roll_type = RollType.STANDARD){
+	const roll = n === 100 ? roll_d100 : get_std_die(n);
+	if(roll_type === RollType.STANDARD) {
+		return roll();
+	}
+	const rolls = [roll(), roll()];
+	if(roll_type === RollType.ADVANTAGE) {
+		return Math.max(...rolls);
+	}
+	if(roll_type === RollType.DISADVANTAGE) {
+		return Math.min(...rolls);
+	}
+	throw `Roll type "${roll_type}" is unknown`;
 }
 
-function roll_die_repeat(reps, n){
+function roll_die_repeat(reps, n, roll_type = RollType.STANDARD){
     let sum = 0;
     for(let i = 0; i < reps; i++)
-        sum += roll_die(n);
+        sum += roll_die(n, roll_type);
     return sum;
 }
 
--- index.js
+++ index.js
@@ -9,12 +9,14 @@ let els = {
     'reps': document.querySelector('#reps'),
     'faces': document.querySelector('#faces'),
     'output': document.querySelector('#output'),
+    'roll_type': document.querySelector('#roll-type'),
 };
 
 function roll(){
     els.output.innerHTML = roll_die_repeat(
         parseInt(els.reps.value),
-        parseInt(els.faces.value)
+        parseInt(els.faces.value),
+		els.roll_type.value
     );
 }
 
